5b086c908d2ddc98c8470b7b6dcc8f58b81c644e
Sascha Homeier <shomeier@apollon.de>
2019-05-30 14:53:22 +0700

    Logging before and after updating Solr index

--- core/src/main/java/jp/aegif/nemaki/cmis/query/solr/SolrSearchEngineService.java
+++ core/src/main/java/jp/aegif/nemaki/cmis/query/solr/SolrSearchEngineService.java
@@ -1,6 +1,10 @@
 package jp.aegif.nemaki.cmis.query.solr;
 
+import java.text.MessageFormat;
+
 import org.apache.chemistry.opencmis.commons.server.CallContext;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -10,12 +14,20 @@ import jp.aegif.nemaki.cmis.aspect.query.solr.SolrUtil;
 @Component
 public class SolrSearchEngineService implements SearchEngineService {
 
+	private static final Log log = LogFactory.getLog(SolrSearchEngineService.class);
+
 	@Autowired
 	private SolrUtil solrUtil;
 
 	@Override
 	public void updateIndex(CallContext callContext) {
+		log.debug(MessageFormat.format("Updating Solr index for repository with id={0} ...",
+				callContext.getRepositoryId()));
+
+		// this call is done synchronously
 		solrUtil.callSolrIndexingNew(callContext.getRepositoryId());
+		log.debug(MessageFormat.format("... successfully updated Solr index for repository with id={0}",
+				callContext.getRepositoryId()));
 	}
 
 }
